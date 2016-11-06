e0c6d52b33b7c45cd8d8af5f259e620bff543b80
Fonda Chu <fonda_chu@yahoo.com>
2016-11-05 23:42:01 -0700

    First commit of app.

---
+++ app/src/main/java/com/example/fonda/simpletodo/EditItemActivity.java
@@ -0,0 +1,37 @@
+package com.example.fonda.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.example.fonda.simpletodo.R.id.etEditText;
+
+public class EditItemActivity extends AppCompatActivity {
+    public final int RESULT_OK = 200;
+    public final String ITEM_ORIGINAL = "originalItem";
+    public final String ITEM_MODIFIED = "modifiedItem";
+    public final String ITEM_POSITION = "itemPosition";
+
+    EditText etEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        String selectedItem = getIntent().getStringExtra(ITEM_ORIGINAL);
+        etEditText = (EditText) findViewById(R.id.etItemToEdit);
+        etEditText.setText(selectedItem);
+    }
+
+    public void onClickSave(View view) {
+
+        Intent data = new Intent();
+        // pass back data as a result
+        data.putExtra(ITEM_MODIFIED, etEditText.getText().toString());
+        data.putExtra(ITEM_POSITION, getIntent().getIntExtra(ITEM_POSITION, -1));
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/fonda/simpletodo/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.fonda.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final String ITEM_ORIGINAL = "originalItem";
+    public final String ITEM_MODIFIED = "modifiedItem";
+    public final String ITEM_POSITION = "itemPosition";
+    public final int REQUEST_CODE = 20;
+    public final int RESULT_OK = 200;
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
+    EditText etEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position,
+                                           long id) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                           long id) {
+                Intent data = new Intent(MainActivity.this, EditItemActivity.class);
+                data.putExtra(ITEM_POSITION, position);
+                data.putExtra(ITEM_ORIGINAL, todoItems.get(position));
+                startActivityForResult(data, REQUEST_CODE);
+            }
+        });
+    }
+
+    public void populateArrayItems() {
+        readItems();
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+            System.out.println("error in readItems");
+            todoItems = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            System.out.println("error in writeItems");
+        }
+    }
+
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            String item = data.getExtras().getString(ITEM_MODIFIED);
+            int pos = data.getExtras().getInt(ITEM_POSITION);
+            if (pos > -1) {
+                todoItems.set(pos, item);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+            }
+        }
+    }
+}
